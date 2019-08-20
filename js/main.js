b9f9e998822367630347d47134315cca98dc9a62
adamc66 <adam.cote66@gmail.com>
2019-08-20 14:29:58 -0400

    completed lightswitch assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    let lightSwitch = document.querySelector('.switch');
+    let theBody = document.querySelector('body')
+    let status = document.querySelector('.status')
+    lightSwitch.addEventListener('click', function(){
+        console.log("I have been clicked");
+        lightSwitch.classList.toggle('on');
+        lightSwitch.classList.toggle('off');
+        theBody.classList.toggle('light')
+        theBody.classList.toggle('dark')
+        if (lightSwitch.classList.contains('off')){
+            status.innerHTML=('Hey! Who turned out the lights!')
+        }else{
+            status.innerHTML=("It's so bright in here!")
+        }
+    })
+
+
+
+
+
+})
 
 
